2b554e061a5f94336cf6c377ad91648c4319a9d8
celiast05 <celia.astier.05@gmail.com>
2025-03-25 15:29:27 +0100

    phase 2 - cartes des voyages dynamiques

--- carte.php
+++ carte.php
@@ -4,7 +4,7 @@ session_start();
 // Durée d'inactivité autorisée
 $timeout = 600; // 10 minutes
 
-if ( !isset($_SESSION['stay_connected'])){ // si Rester connecté n'est pas clické
+if ( !isset($_SESSION['stay_connected'])){ // si Rester connecté n'est pas cliqué
   // Vérifier si l'utilisateur est inactif depuis trop longtemps
   if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
       session_unset(); // Supprime toutes les variables de session
@@ -19,6 +19,19 @@ $_SESSION['last_activity'] = time();
 
 $voyages = json_decode(file_get_contents('json/voyage.json'), true);
 
+$voyageId = $_GET['id'];
+$voyage = null;
+
+foreach ($voyages as $v) {
+    if ($v['id'] == $voyageId) {
+        $voyage = $v;
+        break;
+    }
+}
+
+if ($voyage === null) {
+  die("Voyage introuvable");
+}
 ?>
 
 <!DOCTYPE html>
@@ -55,38 +68,20 @@ $voyages = json_decode(file_get_contents('json/voyage.json'), true);
         }
         ?>
       </div>
-    </nav>
-  <header>
-    <h1>Nos voyages</h1>
-  </header>
-  <main>
-
-    <form name="search" method="post" action="#"> <!--il faut php pour la recherche-->
-      <label for="i1"></label>
-      <input class="search" id="i1" type="text" placeholder="Rechercher...">
-    </form>
-    
-    <br />
-    <br />
-
-	  <div class="container">
-      <?php foreach($voyages as $voyage) { ?>
-      <div class="card">
-        <div class="card-image" style="background-image: url('<?= $voyage['image']; ?>');"></div>
-        <div class="card-content">
-          <h2><?= $voyage['nom']; ?></h2>
-          <p class="category"><?= $voyage['theme']; ?></p>
-          <p class="taille"><?= $voyage['description']; ?></p>
-          <a href="carte.php?id=<?= $voyage['id']; ?>" class="cta">
-            Découvrir
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
-              <path d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <?php } ?>
+</nav>
+<header>
+    <h1>Détails du voyage </h1>
+</header>
+<main>
+    <div class="details">
+    <div class="voyage-image" style="background-image: url('<?php echo $voyage['image']; ?>');"></div>
+    <div class="voyage-info">
+      <h2><?php echo htmlspecialchars($voyage['nom']); ?></h2>
+      <p><strong>Sous-Titre :</strong> <?php echo htmlspecialchars($voyage['theme']); ?></p>
+      <p><strong>Description :</strong> <?php echo htmlspecialchars($voyage['description']); ?></p>
+      <p><strong>Détails :</strong> <?php echo nl2br(htmlspecialchars($voyage['details'])); ?></p>
     </div>
+  </div>
 </main>
 </body>
 </html>
