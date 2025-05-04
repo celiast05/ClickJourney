9a766c9e5616f07b8181cc5b1d659ebb23fc71be
Phy1a <raphaelle.grimaldi@gmail.com>
2025-05-04 19:02:34 +0200

    deleted useless file

--- js/test.js
+++
@@ -1,8 +0,0 @@
-const roles = ["admin", "vip", "bannir", "normal"];
-roles.forEach(function (item, index, array) {
-  if (index == roles.length - 1) {
-    console.log(roles[0]);
-  } else {
-    console.log(roles[index + 1]);
-  }
-});
